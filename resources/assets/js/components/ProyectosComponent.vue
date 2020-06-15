b578281cb56dead4983a62064a887ea996398e5a
Huiza <mejia_huiza@hotmail.com>
2020-06-15 12:39:17 -0600

    Arhivos agregados

---
+++ resources/assets/js/components/ProyectosComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                    <h1>Hola mundo</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <table>
+        <thead>
+
+    </table>
+</template>
+
+<script>
+    export default {
+        data(){
+        return {
+            proyectos:[
+                {id:1,nomProy:
+                "Proyecto x"},
+
+            ]
+
+            }
+        },
+
+        mounted() {
+            axios.get('http://127.0.0.1:8000/proyectos').then(response=>(this.proyectos=response.data))
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
